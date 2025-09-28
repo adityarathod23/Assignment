d86c25bb9bc21aab691a189ecbc07d99c5d3e782
Aditya Rathod <adityarathod148@gmail.com>
2025-09-28 18:53:45 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+# Task 1
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+add=a+b
+print("addition:",add)
+
+sub=a-b
+print("substraction:",sub)
+
+mul=a*b
+print("multiplication:",mul)
+
+div=a/b
+print("division:",div)
+
+
+
+# Task2
+FN=input("Enter your first name:")
+LN=input("Enetr your last name:")
+
+full_name=FN+" "+LN
+
+print(f"\nHello, {full_name}! Welcome to the Python program.")
